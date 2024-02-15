c9f3b2621541da21d7ea18c4b5d86a4b50ecc8ad
Leonardo Lopes <leonardo.lopes@golivetech.com.br>
2024-02-15 20:33:18 -0300

    feat: inserindo imagem no pesquisa e outros ajustes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import { create } from "@wppconnect-team/wppconnect";
+import { MessageType, create } from "@wppconnect-team/wppconnect";
 import { spawn } from "child_process";
-
-
-
-
+import "@wppconnect-team/wppconnect";
+import axios from "axios";
+import fs from "fs";
+import sharp from "sharp";
 create().then((client) => start(client));
 
 const botCommands = {
@@ -11,23 +11,23 @@ const botCommands = {
   oi: "oi",
   tchau: "tchau",
   pesquisa: "pesquisa",
-  menciona: "menciona"
+  menciona: "menciona",
+  fig: "fig",
 };
 
 async function returnAllCommands() {
   let allCommands = "";
   for (const command in botCommands) {
-    allCommands += $`*/{command}*\n`;
+    allCommands += `*-${command}*\n`;
   }
   return allCommands;
 }
-
 
 async function returnPesquisa(query) {
   const pyProg = spawn("python", ["./src/python/pesquisa.py", query]);
 
   return new Promise((resolve, reject) => {
-    let dataBuffer = '';
+    let dataBuffer = "";
 
     pyProg.stdout.on("data", function (data) {
       dataBuffer += data.toString();
@@ -37,7 +37,7 @@ async function returnPesquisa(query) {
       reject(data.toString());
     });
 
-    pyProg.on('close', (code) => {
+    pyProg.on("close", (code) => {
       if (code === 0) {
         resolve(dataBuffer);
       } else {
@@ -46,81 +46,162 @@ async function returnPesquisa(query) {
     });
   });
 }
-async function createMessageFromPesquisa(products) {
-  let message = "";
-
-  if (products && products.length > 0) {
-    for (let i = 0; i < products.length; i++) {
-      message += `*${products[i].name}* \n
-      ${products[i].price}\n
-      ${products[i].parc}\n
-      ${products[i].url}\n
-
-      \n\n`;
-    }
-  }
-
-  return message;
-}
-
-
 
 function start(client) {
   client.onMessage(async (message) => {
-    if (message.body.startsWith("/")){
-      const command = message.body.replace(/^\/(\w+)(?:\s(.*))?/, '$1');
+    if (client == undefined) return;
+    if (message.body.startsWith("-") || message.caption.startsWith("-")) {
+      let command;
+      if (message.type === MessageType.IMAGE) {
+        command = message.caption.replace(/^\-(\w+)(?:\s(.*))?/, "$1");
+      } else {
+        command = message.body.replace(/^\-(\w+)(?:\s(.*))?/, "$1");
+      }
+
       console.log(message.sender.pushname + " enviou " + command);
-      let response;
+
+      let response = "";
       switch (command) {
         case botCommands.oi:
           response = `Olá, tudo bem ${message.sender.pushname}? 😊👋`;
-          await client.sendText(message.from, response).then((result) => {
-            console.log("Result: ", result); //return object success}
-          }).catch((erro) => {  
-            console.error("Error when sending: ", erro); //return object error
-          });
+          await client
+            .sendText(message.from, response)
+            .then((result) => {
+              console.log("Result: ", result); //return object success}
+            })
+            .catch((erro) => {
+              console.error("Error when sending: ", erro); //return object error
+            });
           break;
         case botCommands.tchau:
           response = `Tchau, até logo! 👋`;
           await client.sendText(message.from, response);
           break;
-        case botCommands.comandos:
-          response = await returnAllCommands();
-          await client.reply(
-            message.from,
-            "Lista de comandos:\n" + response
-          );
-          break;
         case botCommands.pesquisa:
           let query = message.body.replace(/^[^ ]+\s+/, "");
           console.log(query);
-          client.sendText(message.from, "Aguarde um momento. Pesquisando sobre " + query + "...");
-          response = await returnPesquisa(query);
-          // response = await createMessageFromPesquisa(response);
-          // console.log(response);
-          await client.sendText(message.from,  JSON.parse(response));
-         
-          await client.sendText(message.from, "Pesquisa concluída.");
-          break;
-        case botCommands.menciona:
-          const participants = await client.message.groupMetadata.participants;
-            
-          let response = '';
-          participants.forEach((participant) => {
-            if (participant.id.user !== 'status@broadcast') {
-              response += `@${participant.id.user.replace(/@c.us/g, '')} `;
+          client.sendText(
+            message.from,
+            "Aguarde um momento. Pesquisando sobre " + query + "..."
+          );
+
+          try {
+            const response = await returnPesquisa(query);
+            console.log("Resposta da pesquisa:");
+            console.log(response);
+
+            // Mapeia os resultados da pesquisa para baixar e enviar imagens simultaneamente
+            const produtos = JSON.parse(response);
+
+            for (const item of produtos) {
+              const imageUrl = item.image_url;
+              const imageName = imageUrl.split("/").pop();
+              const imagePath = `./src/images/${imageName}`;
+
+              try {
+                const imageResponse = await axios.get(imageUrl, {
+                  responseType: "stream",
+                });
+
+                await new Promise((resolve, reject) => {
+                  const imageStream = fs.createWriteStream(imagePath);
+                  imageResponse.data.pipe(imageStream);
+                  imageStream.on("finish", () => {
+                    console.log("Imagem baixada com sucesso!");
+                    resolve();
+                  });
+                  imageStream.on("error", (err) => {
+                    reject(err);
+                  });
+                });
+
+                await client.sendImage(
+                  message.from,
+                  imagePath,
+                  imageName,
+                  `Nome: ${item.name}\nPreço: ${item.price}\nParcelamento: ${item.parc}\nLink para compra: ${item.url}`
+                );
+                fs.unlinkSync(imagePath);
+              } catch (error) {
+                console.error("Erro ao baixar ou enviar a imagem:", error);
+              }
             }
-          });
+
+            await client.sendText(message.from, "Pesquisa concluída.");
+          } catch (error) {
+            console.error("Erro durante a pesquisa:", error);
+            await client.sendText(
+              message.from,
+              "Ocorreu um erro durante a pesquisa."
+            );
+          }
+          break;
+        case botCommands.comandos:
+          response = await returnAllCommands();
           await client.sendText(message.from, response);
           break;
-         
-        // default:
-        //   response = `Desculpe, o comando "${command}" não é reconhecido. Digite /commands para ver a lista de comandos disponíveis.`;
-        //   await client.sendText(message.from, response);
-        //   break;
+        case botCommands.menciona:
+          if (message.isGroupMsg === true) {
+            const participants = await client.getGroupMembers(message.chatId);
+            
+            response = "";
+            client.sendText(message.chatId, "Chamando todos os Cornos do grupo... 🐂🐂")
+            participants.forEach((participant) => {
+              if (participant.id.user !== "status@broadcast") {
+                response += `@${participant.id.user.replace(/@c.us/g, "")} `;
+              }
+            });
+            // Envia a mensagem para o grupo
+            client.sendText(message.chatId, response);
+          } else {
+            response = `Esse comando só pode ser usado em grupos.`;
+            await client.sendText(message.from, response);
+          }
+          break;
+        case botCommands.fig:
+          response = `Criando figurinha...`;
+          await client.sendText(message.from, response);
+
+          if (message.mimetype && message.mimetype.includes("image")) {
+            const mediaData = await client.decryptFile(message);
+
+            const imageName = `figurinha-${message.from}.png`;
+            const imagePath = `./src/images/${imageName}`;
+
+            const resizedImage = await sharp(mediaData)
+              .resize({
+                width: 512,
+                height: 512,
+                fit: "contain", // Manter a proporção original da imagem, ajustando-a para caber completamente dentro das dimensões especificadas
+                background: { r: 255, g: 255, b: 255, alpha: 0 }, // Fundo transparente para a imagem redimensionada
+              })
+              .png()
+              .toBuffer();
+            // Salvar a imagem redimensionada
+            fs.writeFileSync(imagePath, resizedImage);
+
+            const imageBase64 = `data:${
+              message.mimetype
+            };base64,${resizedImage.toString("base64")}`;
+
+            await client.sendImageAsSticker(message.from, imageBase64);
+            fs.unlinkSync(imagePath);
+            break;
+          }
+           else {
+            response = `Você precisa enviar uma imagem para que eu possa criar uma figurinha.`;
+            await client.sendText(message.from, response);
+            break;
+          }
+
+        default:
+          if (message.type != MessageType.IMAGE) {
+            response = `Desculpe, o comando "${command}" não é reconhecido. Digite -comandos para ver a lista de comandos disponíveis.`;
+
+            await client.sendText(message.from, response);
+          }
+          break;
       }
     }
   });
 }
-        
-          
--- src/python/pesquisa.py
+++ src/python/pesquisa.py
@@ -1,12 +1,13 @@
+import sys
+import json
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-import time
-import sys
-import json
-import requests
+
+
+
 class Product:
     def __init__(self, name, price, parc, url, image_url):
         self.name = name
@@ -15,81 +16,69 @@ class Product:
         self.url = url
         self.image_url = image_url
 
-    
 
 
 def search_products_amazon(query):
-    
-    
-    # create a new Chrome browser instance
+
+# Substitua o caminho pelo seu perfil do Chrome
+    profile_path = "C:/Users/leona/AppData/Local/Google/Chrome/User Data"
+
+    # Configurando as opções do Chrome para usar o perfil
     options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    driver = webdriver.Chrome(options=options)
-
-    # navigate to Amazon's homepage
-    driver.get("https://www.amazon.com.br/")
-
-    # wait for the search box to appear
-    wait_for(driver, By.ID, "twotabsearchtextbox")
-
-    # find the search box and search for a product
-    search_box = driver.find_element(By.ID, "twotabsearchtextbox")
+    #options.add_argument('--headless')
+    options.add_argument("user-data-dir=" + profile_path)
+    chrome_path = "C:/Program Files/Google/Chrome/Application/chrome.exe"
+    options.add_argument('--remote-debugging-address=127.0.0.1')
+    options.add_argument('--remote-debugging-port=9222')
     
+    options.binary_location = chrome_path
+    #chromedriver_path = ".\src\pythonchromedriver.exe"
+    driver = webdriver.Chrome(options=options)
+    
+    
+
+    #driver = webdriver.Chrome(options=options)
+    
+    driver.get("https://www.amazon.com.br/")
+    time.sleep(2)
+    
+    wait_for(driver, By.ID, "twotabsearchtextbox")
+    
+    search_box = driver.find_element(By.ID, "twotabsearchtextbox")
     search_box.send_keys(query)
     search_box.submit()
 
-    # wait for the search results to appear
     wait_for(driver, By.ID, "search")
-
-    # print the search results
-    # //div[contains(@class, 's-result-item') and (@data-cel-widget = 'search_result_2')]
     
     products_list = []
     
-    for i in range(2,5):
-        element = driver.find_element(By.XPATH, f"//div[contains(@class, 's-result-item') and (@data-cel-widget = 'search_result_{i}')]")
+    time.sleep(4)
+    for i in range(2, 5):
+        xpath = f"(//div[contains(@class, 's-result-item') and (@data-cel-widget = 'search_result_{i}')])"
+        element = driver.find_element(By.XPATH, xpath)
 
         name = element.find_element(By.CSS_SELECTOR, ".a-size-base-plus.a-color-base.a-text-normal").get_attribute("innerHTML")
         price = element.find_element(By.CSS_SELECTOR, ".a-offscreen").get_attribute("innerHTML").replace("&nbsp;", "")
-        parc_list = element.find_elements(By.CSS_SELECTOR, ".a-color-secondary")
-        
-        for p in parc_list:
-            parc =''.join(p.find_element(By.XPATH, "//span").text.replace("&nbsp;", ""))
        
+        parc_list = element.find_elements(By.XPATH,xpath+"//div[@class='a-row a-size-base a-color-base']/div[2]/span" ) # ".a-color-secondary span"
+        
+        parc = ''.join([p.get_attribute("innerText").replace("&nbsp;", "") for p in parc_list])
+        
         url = element.find_element(By.CSS_SELECTOR, ".a-link-normal.s-underline-text.s-underline-link-text.s-link-style.a-text-normal").get_attribute("href")
         image_url = element.find_element(By.CSS_SELECTOR, ".s-image").get_attribute("src")
 
-        
-        # Criando uma instância da classe Product
         product_instance = Product(name, price, parc, url, image_url)
-
-        # Adicionando a instância à lista
         products_list.append(product_instance)
 
-    
     produtos = json.dumps([product.__dict__ for product in products_list], indent=4)
-       # produtos = str(f"*NOME DO PRODUTO:*: {product_instance.name}\n *Preço*: {product_instance.price}\n *Parcelamento*: {product_instance.parc}\n *Link*: {product_instance.url}\n ImageURL: {product_instance.image_url}\n\n")
-
-        #produtos = ''.join([str(f"*NOME DO PRODUTO:*: {product_instance.name}\n *Preço*: {product_instance.price}\n *Parcelamento*: {product_instance.parc}\n *Link*: {product_instance.url}\n ImageURL: {product_instance.image_url}\n\n") for product_instance in products_list])
     
-    
-    # close the browser
     driver.quit()
     return produtos
 
-
 def wait_for(driver, by, value, timeout=10):
-    """
-    Wait for an element to appear on the page.
-    :param driver: The webdriver instance.
-    :param by: The type of selector to use (e.g. By.ID, By.CLASS_NAME, etc.).
-    :param value: The value of the selector.
-    :param timeout: The maximum amount of time to wait (in seconds).
-    """
     wait = WebDriverWait(driver, timeout)
     wait.until(EC.presence_of_element_located((by, value)))
 
 if __name__ == "__main__":
-
     jsontxt = search_products_amazon(query=sys.argv[1])
-    print(str(jsontxt))
+    print(jsontxt)
